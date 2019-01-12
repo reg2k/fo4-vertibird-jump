6138a12f59ed1a513e6983ddc22a7d7ea0d90c22
reg2k <23437153+reg2k@users.noreply.github.com>
2019-01-12 12:22:59 +0000

    Build for F4SE v0.6.15 (game version v1.10.120)

--- src/Config.h
+++ src/Config.h
@@ -4,8 +4,8 @@
 //-----------------------
 // Plugin Information
 //-----------------------
-#define PLUGIN_VERSION              15
-#define PLUGIN_VERSION_STRING       "1.9.6"
+#define PLUGIN_VERSION              16
+#define PLUGIN_VERSION_STRING       "1.9.7"
 #define PLUGIN_NAME_SHORT           "vertibird_jump"
 #define PLUGIN_NAME_LONG            "Vertibird Jump"
 #define SUPPORTED_RUNTIME_VERSION   CURRENT_RELEASE_RUNTIME
